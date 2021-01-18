30e668ab3f16a69c4eb61813583b0ba8c8fbde43
rOY369 <dishant.arora1996@gmail.com>
2021-01-18 18:18:46 +0530

    organised dir

--- python_design_patterns/Introduction.md
+++
@@ -1,8 +0,0 @@
-## What are Design Patterns ?
-
-A design pattern is a model solution to a common design problem. It describes the problem and a general approach to solving it.
-
-
-<!--stackedit_data:
-eyJoaXN0b3J5IjpbLTE1NDMyODEyNzVdfQ==
--->
---
+++ python_design_patterns/abc_template.py
@@ -0,0 +1,34 @@
+import abc
+
+class MyABC(metaclass=abc.ABCMeta):
+  
+  @abc.abstractmethod
+  def do_something(self):
+    pass
+  
+  @abc.abstractproperty
+  def some_property(self):
+    pass
+  
+  
+class MyConcreteClass(MyABC):
+  
+  def __init__(self, value=None):
+    self._myprop = value
+
+  def do_something(self,value):
+    self._myprop *= 2 + value
+    
+  @property
+  def some_property(self):
+    return self._myprop
+    
+myObject = MyConcreteClass(4)
+myObject.do_something(3)
+print(myObject.some_property)
+
+
+class BadClass(MyABC):
+  pass
+
+bcObject = BadClass()
