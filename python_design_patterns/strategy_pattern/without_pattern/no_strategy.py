5491e249153dc051e120a07e5c9a162376620a5c
rOY369 <dishant.arora1996@gmail.com>
2021-01-19 22:49:36 +0530

    completed no_strategy pattern

--- python_design_patterns/strategy_pattern/without_pattern/no_strategy.py
+++ python_design_patterns/strategy_pattern/without_pattern/no_strategy.py
@@ -1,17 +1,40 @@
 from enum import Enum
 
+
 class Order():
-    
     def __init__(self, shipper):
         self._shipper = shipper
-        
+
     @property
     def shipper(self):
-        returm self._shipper
-        
+        return self._shipper
+
+
 class Shipper(Enum):
     fedex = 1
     ups = 2
     postal = 3
-    
-    
+
+
+class ShippingCost():
+    def shipping_cost(self, order):
+        if order.shipper == Shipper.fedex:
+            return self._fedex_cost
+        elif order.shipper == Shipper.ups:
+            return self._ups_cost
+        elif order.shipper == Shipper.postal:
+            return self._postal_cost
+        else:
+            raise ValueError("invalid shipper")
+
+    @property
+    def _fedex_cost(self):
+        return 3.0
+
+    @property
+    def _ups_cost(self):
+        return 4.0
+
+    @property
+    def _postal_cost(self):
+        return 5.0
