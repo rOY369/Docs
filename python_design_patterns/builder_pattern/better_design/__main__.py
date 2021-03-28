093d4de206c020451c695abeb9ef08838b365ef9
rOY369 <dishant.arora1996@gmail.com>
2021-03-28 20:03:57 +0530

    encapsulating attributes better design

---
+++ python_design_patterns/builder_pattern/better_design/__main__.py
@@ -0,0 +1,7 @@
+from my_computer import MyComputer
+
+builder = MyComputer()
+builder.build_computer()
+
+computer = builder.get_computer()
+computer.display()
---
+++ python_design_patterns/builder_pattern/better_design/my_computer.py
@@ -0,0 +1,15 @@
+from computer import Computer
+
+
+class MyComputer:
+    def get_computer(self):
+        return self._computer
+
+    def build_computer(self):
+        self._computer = computer = Computer()
+        computer.case = "Coolermaster N300",
+        computer.mainboard = "MSI 970",
+        computer.cpu = "Intel Core i7-4770",
+        computer.memory = "Corsair 16GB",
+        computer.hardDrive = "Seagate 1TB",
+        computer.videoCard = "GeForce GTX 1070"
